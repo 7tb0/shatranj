26307d994d204f8353753f0d1f27e0dffa502efd
7tb0 <asdbex1997@gmail.com>
2025-05-16 00:58:05 +0300

    Update assets.js

--- assets.js
+++ assets.js
@@ -6,14 +6,18 @@ export let images = {};
 export function preloadAssets(drawBoardCallback) {
   images = {};
   const { imagePrefix } = getConfig();
-  const prefix = imagePrefix ? `pieces/set${imagePrefix}/` : 'pieces/';
+  // Always look in the pieces/ folder; if the user selected a set number, use pieces/setX/
+  const basePath = imagePrefix
+    ? `pieces/set${imagePrefix}/`
+    : 'pieces/';
+  
   ['R','N','E','F','K','P'].forEach(pt => {
     ['w','b'].forEach(color => {
       const key = color + pt;
       const img = new Image();
       if (drawBoardCallback) img.onload = drawBoardCallback;
-      img.onerror = () => console.warn(`Could not load image: ${prefix}${key}.png`);
-      img.src = `${prefix}${key}.png`;
+      img.onerror = () => console.warn(`Could not load image: ${basePath}${key}.png`);
+      img.src = `${basePath}${key}.png`;
       images[key] = img;
     });
   });
